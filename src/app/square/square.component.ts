8cbf972471f0246d4c137cfba6eeb842632b66b9
Mudiswa Muteba <mudi@pop-os.localdomain>
2020-05-12 01:47:25 +0200

    added square (game tile) component

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+    <p>
+      square works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class SquareComponent {
+
+
+}
